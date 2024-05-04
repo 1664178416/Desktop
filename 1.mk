25a4adf1fd0877532d9b6c60be2d7f5fb70e5b22
LIVE_FOR_C++ <1664178416@qq.com>
2024-05-04 00:59:59 -0700

    xxx

---
+++ 1.mk
@@ -0,0 +1,8 @@
+all:test1 test2
+	echo "test"
+
+test1:
+	echo "test1"
+
+test2:
+	echo "test2"
--- gdb_test/test.cpp
+++ gdb_test/test.cpp
@@ -1,9 +1,17 @@
 #include<stdio.h>
+#include<iostream>
+#include<vector>
+using namespace std;
 
 void func(){
     int i = 0;
     for(i = 0;i < 10;i++){
 	printf("i = %d\n",i);	
+    }
+    vector<int> a = {1,2,3};
+    for(i = 0;i<a.size();i++){
+        printf("vector[%d]:%d",i,a[i]);
+        cout << " " << a[i] << endl;
     }
 }
 
